fe739da0cf72f61d2c3aefc19a9be1e58d7a9cf4
sudarsangp <sudarsangp@gmail.com>
2014-08-24 18:59:44 +0800

    implemented inputparser and unit tests. started to implement main method

--- KWIC/src/CS3213/InputParser.java
+++ KWIC/src/CS3213/InputParser.java
@@ -1,7 +1,31 @@
 package CS3213;
 
+import java.util.ArrayList;
+import java.util.List;
 /**
  * Created by junchao on 8/23/2014.
  */
 public class InputParser {
+    private List<String> _titles;
+
+    public InputParser(){
+        this._titles = new ArrayList<String>();
+    }
+
+    public void addTitle(String input){
+
+        assert(input != null);
+        input = input.toLowerCase();
+        _titles.add(input);
+    }
+
+    public String[] getAllTitles(){
+       return this._titles.toArray(new String[this._titles.size()]);
+    }
+
+    public void printTitles(){
+        for(int i=0; i<_titles.size();i++){
+            System.out.println(_titles.get(i));
+        }
+    }
 }
--- KWIC/src/CS3213/Main.java
+++ KWIC/src/CS3213/Main.java
@@ -1,7 +1,32 @@
 package CS3213;
 
+import java.util.Scanner;
 /**
  * Created by junchao on 8/23/2014.
  */
 public class Main {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        InputParser movieTitles = new InputParser();
+
+        System.out.println("Enter movie titles (terminate input by entering 0) ");
+
+        String userInput = sc.nextLine();
+        while(!userInput.equals("0")){
+            movieTitles.addTitle(userInput);
+            userInput = sc.nextLine();
+        }
+        movieTitles.printTitles();
+
+        System.out.println("Enter words to ignore (terminate input by entering 0) ");
+        String inputWordToIgnore = sc.nextLine();
+        WordsToIgnore wordsToIgnore = WordsToIgnore.getWordsToIgnore();
+        while(!inputWordToIgnore.equals("0")){
+            wordsToIgnore.addWordToIgnore(inputWordToIgnore);
+            inputWordToIgnore = sc.nextLine();
+        }
+
+        System.exit(0);
+    }
 }
