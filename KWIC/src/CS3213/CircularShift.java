8e9df8aea5d3a3a5e80aba3016c62337b186d6f8
franklingu <franklingujunchao@gmail.com>
2014-08-24 12:52:24 +0800

    change in circular shit

--- KWIC/src/CS3213/CircularShift.java
+++ KWIC/src/CS3213/CircularShift.java
@@ -1,18 +1,23 @@
 package CS3213;
 
 /**
- * Created by junchao on 8/23/2014.
+ * This class is to generate a list of all circular shifts based on the string given.
  */
 public class CircularShift {
-    public static String DELIMETER = " ";
+    public static String DELIMITER = " ";
     private String _line;
 
+    /**
+     * input should not be null
+     * @param line
+     */
     public CircularShift(String line) {
+        assert(line != null);
         this._line = line;
     }
 
     public String[] getCircularShifts() {
-        String[] words = this._line.split(DELIMETER);
+        String[] words = this._line.split(DELIMITER);
         String[] shifts = new String[words.length];
         shifts[0] = this._line;
 
@@ -27,11 +32,11 @@ public class CircularShift {
         StringBuilder builder = new StringBuilder();
         for (int i=index;i<words.length;i++) {
             builder.append(words[i]);
-            builder.append(DELIMETER);
+            builder.append(DELIMITER);
         }
         for (int i=0;i<index;i++) {
             builder.append(words[i]);
-            builder.append(DELIMETER);
+            builder.append(DELIMITER);
         }
         if (builder.length() > 0) {
             builder.deleteCharAt(builder.length() - 1);
